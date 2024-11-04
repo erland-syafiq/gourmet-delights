6d55b9ac3f917e50fac6beb19e2716e3c05ce8e4
Erland <syafiqeas@gmail.com>
2024-11-03 21:23:31 -0500

    Added exception support

---
+++ api/src/main/java/com/gourmetdelights/api/exception/GlobalExceptionHandler.java
@@ -0,0 +1,19 @@
+package com.gourmetdelights.api.exception;
+
+import java.util.Date;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<?> ResourceNotFoundException(Exception exception, WebRequest request) {
+        ErrorDetails details = new ErrorDetails(new Date(), exception.getMessage(), request.getDescription(false));
+        return new ResponseEntity<>(details, HttpStatus.NOT_FOUND);
+    }
+}
