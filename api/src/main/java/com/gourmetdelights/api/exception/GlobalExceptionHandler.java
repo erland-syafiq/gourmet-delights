295428b338ea5ab2ea6de9bfa4633534f0cbdb27
Erland <syafiqeas@gmail.com>
2024-11-08 13:05:40 -0500

    Added json error handling

--- api/src/main/java/com/gourmetdelights/api/exception/GlobalExceptionHandler.java
+++ api/src/main/java/com/gourmetdelights/api/exception/GlobalExceptionHandler.java
@@ -8,12 +8,20 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 
+import com.fasterxml.jackson.databind.exc.InvalidFormatException;
+
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(ResourceNotFoundException.class)
-    public ResponseEntity<?> ResourceNotFoundException(Exception exception, WebRequest request) {
+    public ResponseEntity<?> resourceNotFoundException(Exception exception, WebRequest request) {
         ErrorDetails details = new ErrorDetails(new Date(), exception.getMessage(), request.getDescription(false));
         return new ResponseEntity<>(details, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(InvalidFormatException.class)
+    public ResponseEntity<?> invalidFormatException(Exception exception, WebRequest request) {
+        ErrorDetails details = new ErrorDetails(new Date(), exception.getMessage(), request.getDescription(false));
+        return new ResponseEntity<>(details, HttpStatus.BAD_REQUEST);
     }
 }
